880fd97ff1c88343668ca385e82cd9e2f1c8c815
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-16 21:48:26 -0500

    :sparkles: Unsplash
    It's working again

--- wmwc/providers/provider.py
+++ wmwc/providers/provider.py
@@ -1,6 +1,6 @@
 import requests
 import shutil
-
+import os
 
 class Provider:
     def __init__(self, monitor_long, monitor_height, topic, save, url, filename):
@@ -17,3 +17,11 @@ class Provider:
             r.raw.decode_content = True
             with open(self.filename,mode) as f:
                 shutil.copyfileobj(r.raw, f)
+
+    def get_path_image(self):
+            path_to_file = os.path.join(os.getcwd(), self.filename)
+            return path_to_file
+
+
+    def setup_image(self,file_path):
+        os.system(f"feh --bg-fil {file_path}")
--- wmwc/providers/unsplash.py
+++ wmwc/providers/unsplash.py
@@ -1,9 +1,19 @@
 from wmwc.functions.generate_class import generate_class
 from urllib.request import urlopen
+import os
 
-url = urlopen("https://source.unsplash.com/random").geturl()
-filename = url.split("/")[-1]
+def set_class(options):
+    url = urlopen(
+        f"https://source.unsplash.com/random/{options['monitor_long']}x{options['monitor_height']}"
+    ).geturl()
+    global filename
+    filename = "random"
+#    filename = url.split("/")[-1]
+    unsplash = generate_class(options, url, filename)
+    return unsplash
 
 def run(options):
-    unsplash = generate_class(options, url, filename)
+    unsplash = set_class(options)
     unsplash.download("wb")
+    path = unsplash.get_path_image()
+    unsplash.setup_image(path)
