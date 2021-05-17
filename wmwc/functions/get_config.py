29692419914361c430c6b07a6438371f269af5c2
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-17 08:39:20 -0500

    :memo: Reimplement config

--- wmwc/functions/get_config.py
+++ wmwc/functions/get_config.py
@@ -7,4 +7,7 @@ CONFIG_PATH = config.read(expanduser("~/.config/wm-wallpaper-changer/config"))
 
 
 def get_config(secction, option):
-    return config.get(secction, option)
+    try:
+        return config.get(secction, option)
+    except:
+        return "none"
