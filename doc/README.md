a0236cafa4884819ddc73370dbc46788777d4d19
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-18 16:13:30 -0500

    :art: Update

--- doc/README.md
+++ doc/README.md
@@ -3,3 +3,17 @@
 ## Configuration parameters
 
 ## Interesting uses
+- New Wallpaper at startup
+	If you use I3wm, you can put something like this in your i3config:
+	```bash
+exec ~/scripts/random_wallpaper
+	```
+	Similar if you use bspwm, put this in your bspwmrc:
+	```bash
+	python ~/Documents/wmwc/main.py
+	```
+- New Wallpaper at new terminal:
+	Similar to above, put this on your .bashrc or .zshrc, etc:
+	```bash
+	python ~/Documents/wmwc/main.py
+	```
--- wmwc/functions/get_config.py
+++ wmwc/functions/get_config.py
@@ -1,16 +1,16 @@
 from configparser import ConfigParser
-from os.path import expanduser
-import os.path
+from os.path import expanduser, isfile
+
 """ Get User Config """
 config = ConfigParser()
 
-config_path = expanduser("~/.configa/wm-wallpaper-changer/config")
+config_path = expanduser("~/.config/wm-wallpaper-changer/config")
 
-if os.path.isfile(config_path):
+if isfile(config_path):
     CONFIG_PATH = config.read(config_path)
 else:
     # It's runing on GitHub or you didn't create `config_path` :(
-    CONFIG_PATH = config.read("./doc/exampleconfig")
+    CONFIG_PATH = config.read("./doc/if_no_config")
 
 def get_config(secction, option):
     try:
