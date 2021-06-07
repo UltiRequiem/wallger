0fc09e1377541bd02456b57e22798f8a44b7e79a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-07 17:57:08 -0500

    :art: Update

---
+++ check_format.py
@@ -0,0 +1,23 @@
+import os
+
+files = [
+    "wallger/__init__.py",
+    "wallger/__main__.py",
+    "wallger/helpers.py",
+    "wallger/run.py",
+    "wallger/version.py",
+    "wallger/providers/__init__.py",
+    "wallger/providers/local.py",
+    "wallger/providers/provider.py",
+    "wallger/providers/unsplash.py",
+    "wallger/providers/wallhaven.py",
+]
+
+
+def check_format():
+    for i in files:
+        os.system(f"pycodestyle --show-source --show-pep8 --format=default {i}")
+
+
+if __name__ == "__main__":
+    check_format()
--- checkformat.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-files=( "wallger/__init__.py" "wallger/__main__.py" "wallger/helpers.py" "wallger/run.py" "wallger/version.py" "wallger/providers/__init__.py" "wallger/providers/local.py" "wallger/providers/provider.py" "wallger/providers/unsplash.py" "wallger/providers/wallhaven.py")
-for i in "${files[@]}"
-do
-  :
-  pycodestyle --show-source --show-pep8 --format=default "$i"
-done
