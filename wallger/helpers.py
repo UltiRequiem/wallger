3d8fc58f0d6fe86bc2da6a98d38ef901c3b5dbdb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-16 12:11:41 -0500

    :sparkles: Update Manage Errors

--- wallger/helpers.py
+++ wallger/helpers.py
@@ -1,14 +1,19 @@
-import importlib
 import json
 import sys
 
-from .constants import CONFIG_PATH, ERROR_MESSAGE
+from .constants import CONFIG_PATH
+from .ui import cprint
 
 
 def get_config_file() -> dict:
     try:
         with open(CONFIG_PATH) as config:
             return json.load(config)
-    except FileNotFoundError as error:
-        print("Config file not found!")
+    except json.decoder.JSONDecodeError:
+        cprint(" Your configuration is invalid!")
+        sys.exit(0)
+    except FileNotFoundError:
+        cprint("Config file not found!")
+        sys.exit(0)
+    except:
         sys.exit(0)
